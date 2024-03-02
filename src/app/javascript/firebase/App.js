e8bdea8714f9d2ede7d560fbf9d4f7e9e751de27
igor <85964276+igcardoso@users.noreply.github.com>
2024-03-01 21:58:29 -0300

    update src/app/javascript/firebase/App.js

--- src/app/javascript/firebase/App.js
+++ src/app/javascript/firebase/App.js
@@ -12,7 +12,8 @@ import {
     setDoc,
     getDoc,
     getDocs,
-    deleteDoc
+    deleteDoc,
+    updateDoc
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {

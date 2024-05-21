e31e2b50d2fbff64f718f88432b6b92c9296f659
igor <85964276+igcardoso@users.noreply.github.com>
2024-05-21 19:27:59 -0300

    update src/app/javascript/floating-elements.js

--- src/app/javascript/floating-elements.js
+++ src/app/javascript/floating-elements.js
@@ -1,90 +1,28 @@
-document.addEventListener('DOMContentLoaded', function () {
-	const tabs = document.querySelectorAll('.floating-element');
-	const navButtons = document.querySelectorAll('.floating-element-option');
-	const clickAway = document.querySelectorAll('.click-away');
+var menuMore = document.querySelector('.header-more-options');
+const menuHome = document.querySelector('.menu-home');
+const Away = document.querySelectorAll('.scroll');
+const profileHeader = document.querySelector('header img');
+	
+window.disable = function disable() {
+  menuMore.classList.remove('active')
+  Away.forEach(Away => Away.classList.remove('active'));
+  profileHeader.classList.remove('open-opacity');
+  Away.forEach(Away => {
+    Away.style.filter = 'brightness(1)';
+  })
+}
 
-	function showFloatingElement(tabId) {
-		tabs.forEach(tab => tab.classList.remove('active'));
+menuHome.addEventListener('click', ()=> {
+  menuMore.classList.add('active');
+  Away.forEach(Away => Away.classList.add('active'))
+  profileHeader.classList.add('open-opacity');
+  Away.forEach(Away => {
+    Away.style.filter = 'brightness(.6)';
+  })
+})
 
-		const selectedTab = document.getElementById(tabId);
-		selectedTab.classList.add('active');
-		if (selectedTab.classList.contains('floating-element')) {
-			clickAway.forEach(Away => {
-				Away.classList.add('active');
-			});
-		}
-	}
-	//
-	// 	document.addEventListener('click', function(event) {
-	// 		const floatingElement = event.state ? event.state.page: 'home';
-	// 		var element = document.getElementById(floatingElement);
-	// 		var isClickInsideElement = element.contains(event.target);
-	//
-	// 		if (!isClickInsideElement) {
-	// 			history.back();
-	// 		} else {
-	// 			floatingElement.classList.add('active')
-	// 		}
-	// 	});
-	//
-
-	function handleNavClick(event) {
-		const selectedPage = event.currentTarget.getAttribute('data-page');
-		let currentPage = document.getElementById(selectedPage);
-		if (!currentPage.classList.contains('active')) {
-			setTimeout(function() {
-				// Bottom tab
-				showFloatingElement(selectedPage);
-
-				window.history.pushState({
-					page: selectedPage
-				}, null, `#${selectedPage}`);
-			},
-				100);
-			headerButtonBehavior(selectedPage);
-		}
-	}
-
-	navButtons.forEach(button => {
-		button.addEventListener('click', handleNavClick);
-	});
-
-	window.addEventListener('popstate',
-		function (event) {
-			const page = event.state ? event.state.page: 'home';
-			showFloatingElement(page);
-			headerButtonBehavior(page);
-		});
-
-	function functionClickAway() {
-		history.back();
-		clickAway.forEach(Away => {
-			Away.classList.remove('active');
-			const currentVisibleElement = window.history.state ? window.history.state.page: 'home';
-			headerButtonBehavior(currentVisibleElement);
-		});
-	}
-
-	clickAway.forEach(btn => {
-		btn.addEventListener("click", ()=> {
-			functionClickAway();
-		})
-	});
-
-	function headerButtonBehavior(tabId) {
-		const element = tabId
-		const HeaderIconButtonMore = document.querySelector('header .content-profile .icon');
-		const HeaderPhotoButtonMore = document.querySelector('header .content-profile .profile');
-		let IdCurrentElement = document.getElementById(element);
-
-		if (IdCurrentElement.classList.contains('header-more-options')) {
-
-			HeaderIconButtonMore.classList.toggle('hidden');
-			HeaderPhotoButtonMore.classList.toggle('open-opacity');
-		} else if (HeaderIconButtonMore.classList.contains('hidden') && HeaderPhotoButtonMore.classList.contains('open-opacity')) {
-			HeaderIconButtonMore.classList.remove('hidden');
-			HeaderPhotoButtonMore.classList.remove('open-opacity');
-		}
-	}
-
-});
+Away.forEach(Away => {
+  Away.addEventListener('click', ()=> {
+    disable();
+  });
+})

44045ffdd435dab7d9b1f9ea55eb3e74b997e97f
igor <85964276+igcardoso@users.noreply.github.com>
2024-06-02 19:30:26 -0300

    update src/app/javascript/loading.js

--- src/app/javascript/loading.js
+++ src/app/javascript/loading.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#pages-and-header').style.display = 'none';
   setTimeout(function() {
-    document.querySelector('#loading-app p').classList.add('netflix-effect');
-  }, 700);
+    document.querySelector('#loading-app .loading-icon').classList.add('hidden');
+  }, 3000);
 	setTimeout(function() {
-		document.querySelector('#loading-app').style.display = 'none';
+		document.querySelector('#loading-app').style.transform = 'translate(-100%)';
+		// document.querySelector('#loading-app').style.display = 'none';
 		document.querySelector('#pages-and-header').style.display = 'block';
 	}, 4000);
-});
+}); /*o paradoxo coverfild*/

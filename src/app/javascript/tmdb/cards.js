92347f0cdeda72098784cd43077659c854cc2d4c
igor <85964276+igcardoso@users.noreply.github.com>
2025-10-28 00:39:20 -0300

    update src/app/javascript/tmdb/cards.js

--- src/app/javascript/tmdb/cards.js
+++ src/app/javascript/tmdb/cards.js
@@ -607,7 +607,7 @@ btnTvShowSubPage.addEventListener('click', ()=> {
 
 const PgHome = document.querySelector("#home .scroll");
 const backgroundDiv = document.querySelector('.image-background-content');
-const SCROLL_FACTOR = 1;
+const SCROLL_FACTOR = .8;
 
 function handleBackgroundScroll() {
 	const scrollTop = PgHome.scrollTop;

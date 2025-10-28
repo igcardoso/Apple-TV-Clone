fd784d41bbdd8230e1a4dc14c5aea7cf366c3f63
Igor O. Cardoso <iggo.cardoso76@gmail.com>
2025-10-27 21:29:56 -0300

    Mudanças

--- src/app/javascript/tmdb/cards.js
+++ src/app/javascript/tmdb/cards.js
@@ -596,7 +596,7 @@ btnTvShowSubPage.addEventListener('click', ()=> {
 
 const PgHome = document.querySelector("#home .scroll");
 const backgroundDiv = document.querySelector('.image-background-content');
-const SCROLL_FACTOR = 0.1;
+const SCROLL_FACTOR = 0.05;
 
 function handleBackgroundScroll() {
 	const scrollTop = PgHome.scrollTop;
